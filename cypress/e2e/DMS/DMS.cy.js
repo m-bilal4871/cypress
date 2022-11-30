e83a5dbf0a04783171b08895d352a251cfc48369
m-bilal4871 <iammbilal01@gmail.com>
2022-11-30 16:48:15 +0500

    Remove

--- cypress/e2e/DMS/DMS.cy.js
+++ cypress/e2e/DMS/DMS.cy.js
@@ -6,8 +6,7 @@ describe('Document Management System', () => {
       cy.get('[type=text]').type("m.bilal");
       cy.get('[type=password]').type("Bilal@123");
       cy.get('[type=submit]').click(); //cy.get(".grid > :nth-child(1) > .buttons > .w-full").click();
-      //cy.url().should('include','Welcome to Car Planning')
-      //cy.get(".header-heading").contains("Hi Bilal test account, Welcome to Car Planning")  
+      
   
       cy.visit("http://192.168.3.39:9009/DMS/document/Index"); //cy.url().should('include', '/DMS/document/Index')    
   
