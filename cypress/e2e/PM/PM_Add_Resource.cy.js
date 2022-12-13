8759b22c368f565243d92105c76e15752e03cfb3
M.Bill <m.bilal@aircod.com>
2022-12-13 10:46:11 +0500

    Setting and Resources

---
+++ cypress/e2e/PM/PM_Add_Resource.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+describe('Project Management', () => {
+    it('Edit_Project', () => {
+      cy.visit("http://192.168.3.39:9009/");
+      cy.get('[type=text]').type("m.bilal");
+      cy.get('[type=password]').type("Bilal@123");
+      cy.get('[type=submit]').click();
+      cy.url().should('eq', 'http://192.168.3.39:9009/Project/Project/List');
+      cy.wait(4000)      
+      cy.get('[role=presentation]').find('tr').eq(4).find('td').eq(28)
+      cy.get('.dx-datagrid-content-fixed > .dx-datagrid-table > tbody > [aria-rowindex="3"] > [aria-describedby="dx-col-146"] > .icon-btn-only').click({force:true})
+      cy.get('.dropdown-menu.show > ul > :nth-child(1) > a').click()
+      cy.get('.update-name').should('be.visible').contains('Edit Project ');
+
+      cy.get('#lnkPTRClick').click()
+      
+    });
+  });
+  
--- cypress/e2e/PM/PM_Create.cy.js
+++ cypress/e2e/PM/PM_Create.cy.js
@@ -12,9 +12,7 @@ describe('Project Management', () => {
       cy.get('[id=ProjectCode]').type("Auto CP 1");
       cy.get('[id=project_company]').click();
       cy.get('[id=ProjectCompanyTree]').click();
-      cy.contains('.dx-treeview-node-container[role="group"]', 'Dish Communication Inc.').click({
-        force: true
-      });
+      cy.contains('.dx-treeview-node-container[role="group"]', 'Dish Communication Inc.').click({force: true});
       cy.get('[id=EntityTypeId]').click();
       cy.get('.dx-treeview-search > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input[role="textbox"]').type('Site');
       cy.get('[role=group]').should('be.visible');
@@ -23,7 +21,7 @@ describe('Project Management', () => {
       cy.get('input[id=ScheduleType]').type("Project");
       cy.get('[role=listbox]').should('be.visible').contains('[role=option]', 'Project Start Date').scrollIntoView().click();
       cy.get('#StartDate').click().type('Thursday, 24 November, 2022 04:16:00 PM');
-      cy.get('#btnSavePM').click();
+
       cy.wait(2000); //cy.get('.title').should("Success")
   
       cy.get('.title').then(function ($Welcome) {
@@ -33,6 +31,7 @@ describe('Project Management', () => {
       });
       cy.log("Test Case Pass");
     });
+
     it('Draft_Project', () => {
       cy.visit("http://192.168.3.39:9009/");
       cy.get('[type=text]').type("m.bilal");
